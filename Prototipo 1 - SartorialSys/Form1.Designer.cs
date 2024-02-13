1c538b83b2f8a82a2682e5d3701068c798384256
JoseDA0721 <133171655+JoseDA0721@users.noreply.github.com>
2024-02-13 13:00:02 -0500

    Nuebos cambios Mensajes de precuacion

--- Prototipo 1 - SartorialSys/Form1.Designer.cs
+++ Prototipo 1 - SartorialSys/Form1.Designer.cs
@@ -45,26 +45,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(143, 45);
+            this.label1.Location = new System.Drawing.Point(107, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(348, 33);
+            this.label1.Size = new System.Drawing.Size(280, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "TRAJES HIDALGO PLUS";
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(316, 181);
+            this.txtUsuario.Location = new System.Drawing.Point(237, 147);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(137, 22);
+            this.txtUsuario.Size = new System.Drawing.Size(104, 20);
             this.txtUsuario.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(187, 179);
+            this.label2.Location = new System.Drawing.Point(140, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 24);
+            this.label2.Size = new System.Drawing.Size(60, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "Usuario";
             // 
@@ -72,26 +75,29 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(187, 213);
+            this.label3.Location = new System.Drawing.Point(140, 173);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.Size = new System.Drawing.Size(85, 18);
             this.label3.TabIndex = 3;
             this.label3.Text = "Contraseña";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(316, 215);
+            this.textBox2.Location = new System.Drawing.Point(237, 175);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(137, 22);
+            this.textBox2.Size = new System.Drawing.Size(104, 20);
             this.textBox2.TabIndex = 4;
             this.textBox2.UseSystemPasswordChar = true;
             // 
             // btnIniciarSesion
             // 
             this.btnIniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIniciarSesion.Location = new System.Drawing.Point(228, 264);
+            this.btnIniciarSesion.Location = new System.Drawing.Point(171, 214);
+            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(195, 29);
+            this.btnIniciarSesion.Size = new System.Drawing.Size(146, 24);
             this.btnIniciarSesion.TabIndex = 5;
             this.btnIniciarSesion.Text = "Iniciar Sesion";
             this.btnIniciarSesion.UseVisualStyleBackColor = true;
@@ -100,9 +106,10 @@
             // btnSalir
             // 
             this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(266, 308);
+            this.btnSalir.Location = new System.Drawing.Point(200, 250);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(100, 29);
+            this.btnSalir.Size = new System.Drawing.Size(75, 24);
             this.btnSalir.TabIndex = 6;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -113,9 +120,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 360);
+            this.label4.Location = new System.Drawing.Point(10, 292);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.Size = new System.Drawing.Size(90, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "Copyrigth © 2024";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -123,17 +131,18 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Prototipo_1___SartorialSys.Properties.Resources.icono_cliente1;
-            this.pictureBox1.Location = new System.Drawing.Point(296, 98);
+            this.pictureBox1.Location = new System.Drawing.Point(210, 77);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(70, 65);
+            this.pictureBox1.Size = new System.Drawing.Size(52, 53);
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
             // frmInicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 388);
+            this.ClientSize = new System.Drawing.Size(464, 315);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnSalir);
@@ -143,6 +152,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmInicio";
             this.Text = "SartorialSys";
             this.Load += new System.EventHandler(this.frmInicio_Load);
